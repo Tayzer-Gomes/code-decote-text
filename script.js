4366fd8d2627885352f1aedf09d964de394a1bee
Tayzer Gomes <tayzergomes@gmail.com>
2024-02-25 10:44:32 -0300

    Codificador e Decodificador de Texto

---
+++ script.js
@@ -0,0 +1,36 @@
+function processText() {
+    var inputText = document.getElementById("inputText").value.toLowerCase();
+    var option = document.getElementById("option").value;
+    var outputText = "";
+
+    if (option === "encrypt") {
+        outputText = encryptText(inputText);
+    } else {
+        outputText = decryptText(inputText);
+    }
+
+    document.getElementById("outputText").value = outputText;
+}
+
+function encryptText(text) {
+    return text.replace(/e/g, "enter")
+               .replace(/i/g, "imes")
+               .replace(/a/g, "ai")
+               .replace(/o/g, "ober")
+               .replace(/u/g, "ufat");
+}
+
+function decryptText(text) {
+    return text.replace(/enter/g, "e")
+               .replace(/imes/g, "i")
+               .replace(/ai/g, "a")
+               .replace(/ober/g, "o")
+               .replace(/ufat/g, "u");
+}
+
+function copyToClipboard() {
+    var outputText = document.getElementById("outputText");
+    outputText.select();
+    document.execCommand("copy");
+    alert("Texto copiado para a área de transferência!");
+}
